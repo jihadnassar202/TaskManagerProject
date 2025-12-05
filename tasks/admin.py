9200d79118c3210e70b9f00042bbb708f5a4ffe0
jihadNassar202 <jihadNassar202@gmail.com>
2025-12-05 21:46:19 +0300

    now you can login as admin

---
+++ tasks/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'assigned_to', 'created_by', 'due_date', 'created_at')
+    list_filter = ('status', 'due_date', 'created_at')
+    search_fields = ('title', 'description')
+
+
+# Register your models here.
